02b44adbc51be2189133f17d8f19620be3021eee
Lucas Nilsson <lucas.nilsson.2@volvocars.com>
2022-03-11 23:09:16 +0100

    Test: parsing underscores and spaces

--- test/helpers/assert_git.sh
+++ test/helpers/assert_git.sh
@@ -2,6 +2,8 @@
 
 assert_number_of_pull()
 {
+    [ -z "$1" ] && return 1
+    
     EXPECTED="$1"
     PULLS="$(grep "pull" "$TRACKME_IO" | wc -l | cut -d' ' -f1)"
     [ "$PULLS" = "$EXPECTED" ] && return 0 || return 1
@@ -9,6 +11,8 @@ assert_number_of_pull()
 
 assert_number_of_push()
 {
+    [ -z "$1" ] && return 1
+    
     EXPECTED="$1"
     PUSHS="$(grep "push" "$TRACKME_IO" | wc -l | cut -d' ' -f1)"
     [ "$PUSHS" = "$EXPECTED" ] && return 0 || return 1
@@ -16,6 +20,8 @@ assert_number_of_push()
 
 assert_added()
 {
+    [ -z "$1" ] && return 1
+    
     EXPECTED="$1"
     ADDED="$(grep "$EXPECTED" "$TRACKME_IO" | wc -l | cut -d' ' -f1)"
     [ "$ADDED" = "1" ] && return 0 || return 1
