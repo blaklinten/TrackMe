57badb99cb692204cf335e0bcf52528379b3cf5f
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2022-08-26 15:44:47 +0200

    Adding sum test

---
+++ test/tests/unit_diff.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Now
+START_TIME_IN_SEC="$(date +%s)"
+echo "Now is $START_TIME_IN_SEC"
+#4h 30min 22s in the future 
+END_TIME_IN_SEC="$((START_TIME_IN_SEC + 60*60*4 + 60*30 + 22))"
+echo "Future is $END_TIME_IN_SEC"
+
+DIFF=$(diff $START_TIME_IN_SEC $END_TIME_IN_SEC)
+echo "Diff is $DIFF"
+
+
+assert_equals "$DIFF" "4:30:22"
