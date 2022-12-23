bec0deea74c4ac680839a2f86c05f619846f2844
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2022-12-23 23:38:44 +0100

    Validations - TODO: fix duplicate entries

--- clean_up.sh
+++ clean_up.sh
@@ -3,13 +3,17 @@
 WD="./trackmedata"
 cd "$WD/" || exit
 
-find . -type f -not -path './.git*' -not -name "clean_up.sh" -not -name "*.csv" -exec sed -i -e s/\ sön\ /\ Sun\ / {} \;
-find . -type f -not -path './.git*' -not -name "clean_up.sh" -not -name "*.csv" -exec sed -i -e s/\ mån\ /\ Mon\ / {} \;
-find . -type f -not -path './.git*' -not -name "clean_up.sh" -not -name "*.csv" -exec sed -i -e s/\ tis\ /\ Tue\ / {} \;
-find . -type f -not -path './.git*' -not -name "clean_up.sh" -not -name "*.csv" -exec sed -i -e s/\ ons\ /\ Wed\ / {} \;
-find . -type f -not -path './.git*' -not -name "clean_up.sh" -not -name "*.csv" -exec sed -i -e s/\ tor\ /\ Thu\ / {} \;
-find . -type f -not -path './.git*' -not -name "clean_up.sh" -not -name "*.csv" -exec sed -i -e s/\ fre\ /\ Fri\ / {} \;
-find . -type f -not -path './.git*' -not -name "clean_up.sh" -not -name "*.csv" -exec sed -i -e s/\ lör\ /\ Sat\ / {} \;
-
-find . -type f -not -path './.git*' -not -name "clean_up.sh" -not -name "*.csv" -exec \
-  sed -i -e 's/CET//' -e 's/CEST//' -e 's/\([[:alpha:]\{3\}[[:space:]]*[[:digit:]]\{1,2\}[[:space:]]*[[:alpha:]]\{3\}[[:space:]]*\)\([[:digit:]]\{1,2\}:[[:digit:]]\{1,2\}:[[:digit:]]\{1,2\}[[:space:]]*\)\([[:digit:]]\{4\}\)/\1\3 \2/' {} \;
+find . -type f -not -path './.git*' -not -name "*.csv" -exec sed -i \
+  -e 's/\ mån\ /\ Mon\ /' \
+  -e 's/\ tis\ /\ Tue\ /' \
+  -e 's/\ ons\ /\ Wed\ /' \
+  -e 's/\ tor\ /\ Thu\ /' \
+  -e 's/\ fre\ /\ Fri\ /' \
+  -e 's/\ lör\ /\ Sat\ /' \
+  -e 's/\ sön\ /\ Sun\ /' \
+  -e 's/CET//' \
+  -e 's/CEST//' \
+  -e 's/\([[:alpha:]\{3\}[[:space:]]*[[:digit:]]\{1,2\}[[:space:]]*[[:alpha:]]\{3\}[[:space:]]*\)\([[:digit:]]\{1,2\}:[[:digit:]]\{1,2\}:[[:digit:]]\{1,2\}[[:space:]]*\)\([[:digit:]]\{4\}\)/\1\3 \2/' \
+  -e 's/0\([[:digit:]]:\)/\1/' \
+  -e 's/:0\([[:digit:]]\)/:\1/' \
+  {} \;
