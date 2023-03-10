2d223bb075f5ed03f051ce6d06abf7cf03bb77a5
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2023-03-10 14:21:18 +0100

    [Test] Adjust UNIT_UNDER_TEST creation for 0478598

--- test/helpers/create_safe_trackMe.sh
+++ test/helpers/create_safe_trackMe.sh
@@ -8,8 +8,8 @@ export TRACKME_IO="/tmp/trackMeIO"
 sed -e 's/^main$//' \
     -e 's/show()/show_orig()/' \
     -e 's/show "Git: Pulling..." &/show "Git: Pulling..."/' \
-    -e 's/git pull.*/echo pull >> \$TRACKME_IO/' \
-    -e 's/git push.*/echo push >> \$TRACKME_IO/' \
+    -e 's/git pull.*/echo pull >> \$TRACKME_IO; then/' \
+    -e 's/git push.*/echo push >> \$TRACKME_IO; then/' \
     -e 's/git add.*/echo "$NEW_ACTIVITY" >> \$TRACKME_IO/' \
     -e 's/| dmenu -i -p "$INFO"/>> \$TRACKME_IO; echo "$INFO" >> \$TRACKME_IO/' \
     -e 's|export CLIENTS="$HOME/.trackme"|export CLIENTS="/tmp/.trackme"|' \
