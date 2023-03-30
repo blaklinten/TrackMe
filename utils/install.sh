99c44ea459010b8048f3182034a7d3ab78037520
lnilss43 <lucas.nilsson.2@volvocars.com>
2023-03-30 18:00:16 +0200

    Add install script

--- utils/install.sh
+++ utils/install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-if [ -f ./trackMe ];then
-  TRACKME_SCRIPT="./trackMe"
-elif [ -f ../trackMe ];then
-  TRACKME_SCRIPT="../trackMe"
+if [[ "$(git branch | grep "\*")" =~ "dmenu" ]]; then
+  INSTALL_DIR="usr/local/bin/"
+else
+  INSTALL_DIR="../../../usr/bin/"
 fi
 
-cp "$TRACKME_SCRIPT" /usr/local/bin/trackMe
+if [ -f ./trackMe ];then
+  TRACKME_SCRIPT="./trackMe"
+  INSTALL_DIR_PREFIX=""
+elif [ -f ../trackMe ];then
+  TRACKME_SCRIPT="../trackMe"
+  INSTALL_DIR_PREFIX=".."
+fi
+
+cp "$TRACKME_SCRIPT" "$INSTALL_DIR_PREFIX/$INSTALL_DIR/trackMe"
