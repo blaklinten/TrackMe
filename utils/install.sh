74683b83ea95237cc4ae6009628209b3bea21d82
lnilss43 <lucas.nilsson.2@volvocars.com>
2023-05-12 13:27:07 +0200

    Fix separate install script for different branches

--- utils/install.sh
+++ utils/install.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-BRANCH="$(git branch | grep "\*")" 
-if [[ "$BRANCH" =~ "dmenu" ]]; then
-  INSTALL_DIR="/usr/local/bin/"
-else
-  INSTALL_DIR="../../../usr/bin/"
-fi
-
 if [ -f ./trackMe ];then
   TRACKME_SCRIPT="./trackMe"
-  INSTALL_DIR_PREFIX=""
 elif [ -f ../trackMe ];then
   TRACKME_SCRIPT="../trackMe"
-  INSTALL_DIR_PREFIX=".."
+else
+  echo "Could not find executable in ./ or ../ - exiting"
+  exit 1
 fi
 
-sudo cp "$TRACKME_SCRIPT" "${INSTALL_DIR_PREFIX}${INSTALL_DIR}/trackMe"
+INSTALL_DIR="/usr/local/bin/"
+sudo cp -v "$TRACKME_SCRIPT" "${INSTALL_DIR}trackMe"
 
-GIT_TRACKME_HASH="$(sha256sum "$TRACKME_SCRIPT" | cut -d' ' -f2)"
-INSTALLED_TRACKME_HASH="$(sha256sum ${INSTALL_DIR}${TRACKME_SCRIPT} | cut -d' ' -f2)"
+if ! GIT_TRACKME_HASH="$(sha256sum "$TRACKME_SCRIPT" | cut -d' ' -f1)"; then
+  printf 'Checksum creation for %s failed\n' "$TRACKME_SCRIPT"
+fi
+
+if ! INSTALLED_TRACKME_HASH="$(sha256sum "${INSTALL_DIR}trackMe" | cut -d' ' -f1)"; then
+  printf 'Checksum creation for %s failed\n' "${INSTALL_DIR}trackMe"
+fi
+
+if [ -z "$GIT_TRACKME_HASH" ] || [ -z "$INSTALLED_TRACKME_HASH" ]; then
+  printf ' Either Git hash <%s> or Installed hash <%s> is empty - exiting\n' "$GIT_TRACKME_HASH" "$INSTALLED_TRACKME_HASH"
+  exit 1
+fi
 
 if [ "$GIT_TRACKME_HASH" = "$INSTALLED_TRACKME_HASH" ];then
+  printf '%s\n%s\n%s\n' "Hashes are:" "$GIT_TRACKME_HASH" "$INSTALLED_TRACKME_HASH"
   echo "Successfully installed latest version from Git."
 else
   echo "Hashes don't match - latest version not installed."
