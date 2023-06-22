55b6888fb2ea3b70a8965d4f429e25d3248fb272
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2023-06-22 14:44:09 +0200

    Updating install script to also work for Termux

--- utils/install.sh
+++ utils/install.sh
@@ -14,27 +14,41 @@ if ! GIT_TRACKME_HASH="$(sha256sum "$TRACKME_SCRIPT" | cut -d' ' -f1)"; then
   exit 1
 fi
 
-INSTALL_DIR="/usr/local/bin/"
-if ! CURRENTLY_INSTALLED_TRACKME_HASH="$(sha256sum "${INSTALL_DIR}trackMe" | cut -d' ' -f1)"; then
-  printf 'Checksum creation for %s failed\n' "${INSTALL_DIR}trackMe"
-  exit 1
+TERMUX_DATA_DIR="/data/data/com.termux/files/usr/bin"
+if [ -d "$TERMUX_DATA_DIR" ]; then # This is Termux, i.e. Android phone
+  INSTALL_DIR="$TERMUX_DATA_DIR"
+  USE_SUDO="no"
+else # We are on laptop
+  INSTALL_DIR="/usr/local/bin"
+  USE_SUDO="yes"
 fi
 
-if [ -z "$GIT_TRACKME_HASH" ] || [ -z "$CURRENTLY_INSTALLED_TRACKME_HASH" ]; then
-  printf ' Either Git hash <%s> or Installed hash <%s> is empty - exiting\n' "$GIT_TRACKME_HASH" "$CURRENTLY_INSTALLED_TRACKME_HASH"
-  exit 1
+if [ -f "${INSTALL_DIR}/trackMe" ];then
+  if ! CURRENTLY_INSTALLED_TRACKME_HASH="$(sha256sum "${INSTALL_DIR}/trackMe" | cut -d' ' -f1)"; then
+    printf 'Checksum creation for %s failed\n' "${INSTALL_DIR}/trackMe"
+    exit 1
+  fi
+
+  if [ -z "$GIT_TRACKME_HASH" ] || [ -z "$CURRENTLY_INSTALLED_TRACKME_HASH" ]; then
+    printf ' Either Git hash <%s> or Installed hash <%s> is empty - exiting\n' "$GIT_TRACKME_HASH" "$CURRENTLY_INSTALLED_TRACKME_HASH"
+    exit 1
+  fi
+
+  printf 'Installed version hash: %s\n      Git version hash: %s\n' "$CURRENTLY_INSTALLED_TRACKME_HASH" "$GIT_TRACKME_HASH"
+  if [ "$GIT_TRACKME_HASH" = "$CURRENTLY_INSTALLED_TRACKME_HASH" ]; then
+    echo "Current version already installed, nothing to do"
+    exit 0
+  fi
 fi
 
-printf 'Installed version hash: %s\n      Git version hash: %s\n' "$CURRENTLY_INSTALLED_TRACKME_HASH" "$GIT_TRACKME_HASH"
-if [ "$GIT_TRACKME_HASH" = "$CURRENTLY_INSTALLED_TRACKME_HASH" ]; then
-  echo "Current version already installed, nothing to do"
-  exit 0
+if [ "$USE_SUDO" = "yes" ];then
+  sudo cp -v "$TRACKME_SCRIPT" "${INSTALL_DIR}/trackMe"
+else
+  cp -v "$TRACKME_SCRIPT" "${INSTALL_DIR}/trackMe"
 fi
 
-sudo cp -v "$TRACKME_SCRIPT" "${INSTALL_DIR}trackMe"
-
-if ! CURRENTLY_INSTALLED_TRACKME_HASH="$(sha256sum "${INSTALL_DIR}trackMe" | cut -d' ' -f1)"; then
-  printf 'Checksum creation for %s failed\n' "${INSTALL_DIR}trackMe"
+if ! CURRENTLY_INSTALLED_TRACKME_HASH="$(sha256sum "${INSTALL_DIR}/trackMe" | cut -d' ' -f1)"; then
+  printf 'Checksum creation for %s failed\n' "${INSTALL_DIR}/trackMe"
   exit 1
 fi
 
